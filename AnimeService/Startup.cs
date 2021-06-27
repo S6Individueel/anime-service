75c9d53adee87b83aa1fdb0e7556a92952b7559c
I384733 <I384733@fhict.nl>
2021-06-28 00:05:30 +0200

    swagger add

--- AnimeService/Startup.cs
+++ AnimeService/Startup.cs
@@ -58,7 +58,7 @@ namespace AnimeService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
